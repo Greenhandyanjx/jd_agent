948a4ef81413b762602d037f27ff71188fe08e11
witcher <1727494692@qq.com>
2026-05-08 16:05:03 +0800

    修改项目部署和启动错误

--- model/factory.py
+++ model/factory.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import Optional
 from langchain_core.embeddings import Embeddings
+from langchain_community.chat_models.tongyi import BaseChatModel
 
 from langchain_community.embeddings import DashScopeEmbeddings
 from langchain_community.chat_models.tongyi import ChatTongyi
